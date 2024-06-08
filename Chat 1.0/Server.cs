d78656fef28b4bea97c93276e0a6f1d4535bedca
ArtHoly26 <112950470+ArtHoly26@users.noreply.github.com>
2024-06-08 12:33:03 +0200

    Add project files.

---
+++ Chat 1.0/Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ChatServer
+{
+    private static List<TcpClient> clients = new List<TcpClient>();
+    private static TcpListener listener;
+    private static readonly object lockObj = new object();
+
+    static void Main(string[] args)
+    {
+        listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Сервер запущен...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            lock (lockObj)
+            {
+                clients.Add(client);
+            }
+            Console.WriteLine("Клиент подключен...");
+            Thread thread = new Thread(HandleClient);
+            thread.Start(client);
+        }
+    }
+
+    private static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        try
+        {
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                Console.WriteLine("Получено сообщение: " + message);
+                Broadcast(message, client);
+            }
+        }
+        finally
+        {
+            lock (lockObj)
+            {
+                clients.Remove(client);
+            }
+            client.Close();
+        }
+    }
+
+    private static void Broadcast(string message, TcpClient excludeClient)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+        lock (lockObj)
+        {
+            foreach (var client in clients)
+            {
+                if (client != excludeClient)
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
---
+++ Client_1/Client1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ChatClient
+{
+    private static TcpClient client;
+    private static NetworkStream stream;
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            client = new TcpClient("127.0.0.1", 5000);
+            stream = client.GetStream();
+            Console.WriteLine("Подключен к серверу...");
+
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+
+            SendMessages();
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+
+    private static void ReceiveMessages()
+    {
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        try
+        {
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                Console.WriteLine("Сообщение от сервера: " + message);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ошибка при получении сообщений: " + e.Message);
+        }
+    }
+
+    private static void SendMessages()
+    {
+        Console.WriteLine("Введите сообщение:");
+
+        try
+        {
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ошибка при отправке сообщения: " + e.Message);
+        }
+    }
+}
